ae3c3d889ec23b50b3023dc3edc400e4625212e7
rttitity <jinwoo25803@gmail.com>
2024-08-01 10:11:07 +0000

    Update main

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from starlette.responses import PlainTextResponse
 
 app = FastAPI()
 
-@app.get("/health")async def health_check():
+@app.get("/health")
+async def health_check():
     return PlainTextResponse("건강합니다", status_code=200)
-
-@app.get("/")
-async def health_check1():
-    return PlainTextResponse("CI/CD가 성공했습니다",status_code=200)
